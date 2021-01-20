44c94ea57885de1cf4e2701ae66aa13f103e541d
Rujhaan Bhatia <33410430+rujhaan123@users.noreply.github.com>
2021-01-20 23:29:51 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,19 @@
-print('Hello, world!')
-print("Adding the 2nd one")
+class Bank_Account():
+    
+    def __init__(self,owner,balance=0):
+        self.owner=owner
+        self.balance=balance
+        
+    def deposit(self,add):
+        self.balance+=add
+        print('Your deposit is accepted')
+        return self.balance 
+        
+    def withdraw(self,sub):
+        self.balance-=sub
+       
+        if(self.balance<sub):
+            print('Insufficient funds')
+        else:
+            print('withdrawn successful')
+            return self.balance
